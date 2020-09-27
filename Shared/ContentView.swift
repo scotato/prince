127ec2af33404870ab540a80ec5096b9b8e6d705
Scott Dodge <scott@goldilocks.design>
2020-09-26 21:20:17 -0500

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Scott Dodge on 9/26/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/princeApp.swift
@@ -0,0 +1,17 @@
+//
+//  princeApp.swift
+//  Shared
+//
+//  Created by Scott Dodge on 9/26/20.
+//
+
+import SwiftUI
+
+@main
+struct princeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
